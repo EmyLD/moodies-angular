1d1ea036904c7a9bc728bb6d5dc8a6bcbca92eb7
Emy <145982137+EmyLD@users.noreply.github.com>
2024-08-29 18:47:01 +0200

    added particules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ParticulesComponent } from './reusable/particules/particules.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ParticulesComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
+  particles = Array.from({ length: 20 }, (_, i) => ({
+    id: i,
+    left: `${Math.random() * 100}%`,
+    top: `${Math.random() * 100}%`,
+    animationDuration: `${10 + Math.random() * 20}s`, // Variation plus grande pour durée
+    animationDelay: `${Math.random() * 5}s`,
+    transformOrigin: `${Math.random() * 100}% ${Math.random() * 100}%`,
+  }));
 }
