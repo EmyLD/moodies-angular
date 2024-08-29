0b885386909a03bc38d2118da8b30a07f57e9d71
EmyLD <emeline.ledu@outlook.fr>
2024-08-29 15:34:33 +0200

    added tailwind

---
+++ src/app/particules/particules.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-particules',
+  standalone: true,
+  imports: [],
+  templateUrl: './particules.component.html',
+  styleUrl: './particules.component.scss',
+})
+export class ParticulesComponent {}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts, scss}", // Inclure tous les fichiers HTML et TypeScript dans le répertoire src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
