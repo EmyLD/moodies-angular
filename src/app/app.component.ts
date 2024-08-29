8f265ac4ca1d1b7006c93f916be9901954733375
Emy <145982137+EmyLD@users.noreply.github.com>
2024-08-29 19:03:30 +0200

    added reusable button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './reusable/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
-  particles = Array.from({ length: 20 }, (_, i) => ({
+  particles = Array.from({ length: 30 }, (_, i) => ({
     id: i,
     left: `${Math.random() * 100}%`,
     top: `${Math.random() * 100}%`,
@@ -19,4 +20,5 @@ export class AppComponent {
     animationDelay: `${Math.random() * 5}s`,
     transformOrigin: `${Math.random() * 100}% ${Math.random() * 100}%`,
   }));
+  label: string = 'Commencez à écrire';
 }
