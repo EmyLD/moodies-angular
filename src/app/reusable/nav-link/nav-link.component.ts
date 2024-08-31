68b69e385f22fee6c39ce8cc9ef4d2a0ef74aafd
EmyLD <emeline.ledu@outlook.fr>
2024-08-31 11:28:27 +0200

    chnage to routerLink

--- src/app/reusable/nav-link/nav-link.component.ts
+++ src/app/reusable/nav-link/nav-link.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-link',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav-link.component.html',
   styleUrl: './nav-link.component.scss',
 })
---
+++ src/datas/users.ts
@@ -0,0 +1,12 @@
+export const users = [
+  { name: 'Alice Dupont', title: "L'Éternelle Mélancolique", emoji: '😔' },
+  { name: 'Bernard Martin', title: 'Le Réfléchi', emoji: '🤔' },
+  { name: 'Claire Leblanc', title: 'La Passionnée', emoji: '🔥' },
+  { name: 'David Dubois', title: 'Le Créatif', emoji: '🎨' },
+  { name: 'Émilie Garnier', title: 'La Rêveuse', emoji: '🌙' },
+  { name: 'François Moreau', title: 'Le Stoïque', emoji: '😐' },
+  { name: 'Gabrielle Durand', title: "L'Aventurière", emoji: '🌍' },
+  { name: 'Henri Lefèvre', title: 'Le Visionnaire', emoji: '🔭' },
+  { name: 'Isabelle Bertrand', title: 'La Philosophe', emoji: '📚' },
+  { name: 'Julien Petit', title: 'Le Poète', emoji: '📝' },
+];
